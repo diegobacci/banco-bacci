a63f4be12cdb009e35332a4194c777067ad74bb4
Diego Bacci da Silva Barbosa <diegobarbosa.sb@gmail.com>
2021-03-06 14:13:55 -0300

    teste inicial

--- src/bacci/banco/NotificacaoCliente.java
+++ src/bacci/banco/NotificacaoCliente.java
@@ -2,7 +2,7 @@ package bacci.banco;
 
 public class NotificacaoCliente { // Classe destinada a criacao de metodos para notificar clientes de avisos/promocoes
     
-    private char mensagem; 
+    private char mensagem; // QUEM CRIA CONTA CORRENTE GANHA 50 R$ E QUEM CRIA POUPANCA 150 R$
     
     private char aviso() {
         return this.getMensagem();
