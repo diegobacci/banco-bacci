d818ce4c8efc8ffdbfbb194f8ee84bc3a3c53af7
Diego Bacci da Silva Barbosa <diegobarbosa.sb@gmail.com>
2021-03-08 14:55:38 -0300

    Delete Interface.java

--- src/bacci/banco/Interface.java
+++
@@ -1,8 +0,0 @@
-package bacci.banco;
-
-public class Interface {
-    public void menuPrincipal() {
-        System.out.println("BEM-VINDO AO BANCO BACCI");
-        System.out.println("");
-    }
-}
